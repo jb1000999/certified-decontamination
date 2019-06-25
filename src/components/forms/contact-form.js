9022eb919cf15f602b1282854e532b963298d7c1
Jacob Batterman <ctrjcb@gmail.com>
2019-06-25 14:24:20 -0600

    changed post request

--- src/components/forms/contact-form.js
+++ src/components/forms/contact-form.js
@@ -26,7 +26,7 @@ class ContactForm extends Component {
     };
 
     axios
-      .post ('emailer-for-cd-bjyft496r.now.sh/api/v1', data)
+      .post ('https://emailer-for-cd-git-master.ctrjcb.now.sh/api/v1', data)
       .then (res => {
         this.setState ({sent: true}, this.resetForm ());
       })
