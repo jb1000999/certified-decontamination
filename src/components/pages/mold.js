23ba7f305d6e5d4c2a27346b4afe2d1bff10df0b
Jacob Batterman <ctrjcb@gmail.com>
2019-07-18 05:54:49 -0600

    added content to mold page and styling for list

--- src/components/pages/mold.js
+++ src/components/pages/mold.js
@@ -5,8 +5,35 @@ import HomeServices from '../home-content/services-component';
 export default class Mold extends Component {
   render () {
     return (
-      <div>
-        <h1>Mold</h1>
+      <div className="info-page-container">
+        <div className="info-banner">
+          <h1>Mold</h1>
+        </div>
+        <div className="info-container">
+          <div className="info">
+            <p>
+              Most of the calls we receive are from people who do not need a professional mold remediation - they just
+              need a little advice so they can handle the problem themselves. The EPA recommends homeowners
+              handle mold cleanup where less than 10 square feet of mold colonies are present.
+            </p>
+            <p>
+              We follow the old, proven and true decontamination protocol for mold, which is:
+            </p>
+          </div>
+
+          <div className="info-list">
+            <ol type="1">
+              <li>
+                Control the air so the mold spores do not contaminate unaffected areas.
+              </li>
+              <li>Remove the mold colonies.</li>
+              <li>
+                Clean the affected areas to remove residual spores (dust) as feasible.
+              </li>
+              <li>Check the work by inspecting and testing.</li>
+            </ol>
+          </div>
+        </div>
 
         <div className="services-component">
           <HomeServices />
