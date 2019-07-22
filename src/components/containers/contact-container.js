edf48e96c9ec2b6a729607fd6264c580a96567f7
jb1000999 <ctrjcb@gmail.com>
2019-07-22 02:56:17 -0600

    refactored code

--- src/components/containers/contact-container.js
+++ src/components/containers/contact-container.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 
-export default function ContactContainer () {
+export default function ContactContainer() {
   return (
     <div className="call-us-container">
       <h1>Call us: </h1>
-      <div className="phone-number">
-        Call us at (801)809-6932
-      </div>
+      <div className="phone-number">Call us at (801)809-6932</div>
       <div className="location">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3028.673038777485!2d-111.97504768428963!3d40.61503695147812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87528ec22d0ba99d%3A0xa4fb7875ec7b80f7!2sCertified+Decontamination!5e0!3m2!1sen!2sus!4v1561588742627!5m2!1sen!2sus"
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-export default function About () {
+export default function About() {
   return (
     <div className="about-us-container">
       <div className="about-banner">
@@ -8,22 +8,41 @@ export default function About () {
       </div>
       <div className="about-us-content">
         <p>
-          Michael “Mike” Rowzee learned to test and decontaminate in the U.S. Army where he managed chemical,
-          biological and nuclear weapons defense programs.
+          Michael “Mike” Rowzee learned to test and decontaminate in the U.S.
+          Army where he managed chemical, biological and nuclear weapons defense
+          programs.
         </p>
         <br />
         <p>
-          Mike purchased a home in October, 1999 that was closed by the health department. The owner was caught with a meth lab in the garage. Mike decided, “I’ll decontaminate the house as if it were contaminated with nerve gas.” After Mike decontaminated the home and provided the health department with a decontamination report, the health department inspector said, “This is the best decontamination I’ve seen; why don’t you put your name on our contractor list.”
+          Mike purchased a home in October, 1999 that was closed by the health
+          department. The owner was caught with a meth lab in the garage. Mike
+          decided, “I’ll decontaminate the house as if it were contaminated with
+          nerve gas.” After Mike decontaminated the home and provided the health
+          department with a decontamination report, the health department
+          inspector said, “This is the best decontamination I’ve seen; why don’t
+          you put your name on our contractor list.”
         </p>
         <br />
         <p>
-          Mike registered his business name with the State of Utah and his name was added to the health department’s list. Mike received request to decontaminate meth labs and, to his surprise, started receiving requests to decontaminate for mold. After extensive research, Mike began decontaminating homes with mold too. Requests for decontamination didn’t stop with just meth and mold.
+          Mike registered his business name with the State of Utah and his name
+          was added to the health department’s list. Mike received request to
+          decontaminate meth labs and, to his surprise, started receiving
+          requests to decontaminate for mold. After extensive research, Mike
+          began decontaminating homes with mold too. Requests for
+          decontamination didn’t stop with just meth and mold.
         </p>
         <br />
         <p>
-          Mike and company have decontaminated homes, commercial buildings, vehicles and outdoor soil for a variety of other contaminants such as bacteria, hantavirus, tear gas, fentanyl and ricin. In fact, Certified Decontamination is one of the few companies in the World to decontaminate ricin. The first ricin decontamination was in Utah and the other in Pennsylvania. Decontamination is not the only service provided by Certified Decontamination. They inspect and test for chemical and microbiological contaminants and Mike provides consulting services to law firms regarding meth and mold.
+          Mike and company have decontaminated homes, commercial buildings,
+          vehicles and outdoor soil for a variety of other contaminants such as
+          bacteria, hantavirus, tear gas, fentanyl and ricin. In fact, Certified
+          Decontamination is one of the few companies in the World to
+          decontaminate ricin. The first ricin decontamination was in Utah and
+          the other in Pennsylvania. Decontamination is not the only service
+          provided by Certified Decontamination. They inspect and test for
+          chemical and microbiological contaminants and Mike provides consulting
+          services to law firms regarding meth and mold.
         </p>
-
       </div>
     </div>
   );
